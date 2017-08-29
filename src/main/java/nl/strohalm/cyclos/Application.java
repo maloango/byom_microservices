9079558dee00f5527023b04a0463c36bec3c2ccc
Serge <smbikina@gmail.com>
2017-08-29 12:47:05 +0200

    Added Swagger and springfox

--- src/main/java/nl/strohalm/cyclos/Application.java
+++ src/main/java/nl/strohalm/cyclos/Application.java
@@ -5,7 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @ImportResource(value = {
         "classpath:nl/strohalm/cyclos/spring/dao.xml",
@@ -21,8 +30,33 @@ import org.springframework.context.annotation.ImportResource;
 })
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
 @EnableDiscoveryClient
+@EnableSwagger2
+@ComponentScan
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+
+    @Bean
+    public Docket newsApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+    @Bean
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot Aaxon REST Sample with Swagger")
+                .description("Spring Boot Aaxon REST Sample with Swagger")
+                .contact("Sravan Kumar")
+                .license("Apache License Version 2.0")
+                .build();
+    }
+
 }
+
+
